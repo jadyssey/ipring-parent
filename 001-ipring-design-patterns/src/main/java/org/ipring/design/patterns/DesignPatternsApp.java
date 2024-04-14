54970ad5f6a08818cd89f2c0447e63a393f79a19
liuguangjin <1430826258@qq.com>
2024-04-14 11:36:47 +0800

    refact: 初始化项目

---
+++ 001-ipring-design-patterns/src/main/java/org/ipring/design/patterns/DesignPatternsApp.java
@@ -0,0 +1,15 @@
+package org.ipring.design.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author lgj
+ * @Date 2024/4/14
+ */
+@SpringBootApplication
+public class DesignPatternsApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsApp.class, args);
+    }
+}
