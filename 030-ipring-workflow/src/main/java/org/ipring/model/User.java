18aeb2fc6a4a87dd19cf509f15083a0c1f2bf59c
liuguangjin <1430826258@qq.com>
2024-07-10 21:52:54 +0800

    feat[workflow]：新增功能

---
+++ 030-ipring-workflow/src/main/java/org/ipring/model/User.java
@@ -0,0 +1,14 @@
+package org.ipring.model;
+
+import lombok.Data;
+
+/**
+ * @Author lgj
+ * @Date 2024/7/10
+ */
+@Data
+public class User {
+    private String userID;
+    private String name;
+    private String pswd;
+}
