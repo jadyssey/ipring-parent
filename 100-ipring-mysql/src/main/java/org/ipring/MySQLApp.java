28bf9acee651e67d04c5a56d24b1aa035ede11de
liuguangjin <1430826258@qq.com>
2024-07-11 21:03:31 +0800

    feat: 优化项目

---
+++ 100-ipring-mysql/src/main/java/org/ipring/MySQLApp.java
@@ -0,0 +1,15 @@
+package org.ipring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author lgj
+ * @Date 2024/7/11
+ */
+@SpringBootApplication
+public class MySQLApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MySQLApp.class, args);
+    }
+}
